69f89f587410272ccb55ec084cfb33ff2a0bd69f
Ayleen <ayleenpiteotarpy@gmail.com>
2025-05-28 12:26:13 -0700

    update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,26 @@
 import '../styles/Header.css';
+import Logo from '../assets/logo.png';
 
 function Header() {
   return (
     <div className="header">
-        
+      <div className="header-left">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="logo header-img"
+        />
+      </div>
+
+      <div className="header-right">
+        <h1
+          className="my-recipes"
+        >
+          My Recipes
+        </h1>
+        <i className="header-icon fa-solid fa-circle-plus"></i>
+        <i className="profile-icon fa-solid fa-circle-user"></i>
+      </div>
     </div>
   );
 }
