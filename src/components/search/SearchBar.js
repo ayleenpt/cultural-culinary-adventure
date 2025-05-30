3e5d904e88011de624d04d9dce2a29a89a0c09d1
Ayleen <ayleenpiteotarpy@gmail.com>
2025-05-30 13:16:37 -0700

    sort by

--- src/components/search/SearchBar.js
+++ src/components/search/SearchBar.js
@@ -1,6 +1,47 @@
 import '../../styles/search/SearchBar.css';
+import { useState } from 'react';
+import Select from 'react-select';
+
+const options = [
+  {value: 'Favorited_Desc', label: (<>Favorited <i className="fa-solid fa-arrow-down"></i></>)},
+  {value: 'Favorited_Asc', label: (<>Favorited <i className="fa-solid fa-arrow-up"></i></>)},
+  {value: 'Date_Desc', label: (<>Date Posted <i className="fa-solid fa-arrow-down"></i></>)},
+  {value: 'Date_Asc', label: (<>Date Posted <i className="fa-solid fa-arrow-up"></i></>)}
+];
+
+const customComponents = {
+  DropdownIndicator: () => null,
+  IndicatorSeparator: () => null,
+};
 
 function SearchBar() {
+  const [selectedOption, setSelectedOption] = useState(options[0]);
+
+  const customStyles = {
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isFocused ? '#a40000' : 'white',
+      color: state.isFocused ? 'white' : 'black',
+    }),
+    control: (provided) => ({
+      ...provided,
+      minHeight: '100%',
+      height: '100%',
+      minWidth: '170px',
+      width: '170px',
+      border: 'none',
+      boxShadow: 'none',
+      background: 'none',
+      cursor: 'pointer',
+      padding: '0 20px 0 0',
+    }),
+    valueContainer: (provided) => ({
+      ...provided,
+      padding: '0 0px',
+      height: '100%',
+    }),
+  };
+
   return (
     <div className="search-container">
       <form action="/action_page.php" className="search-form">
@@ -14,6 +55,15 @@ function SearchBar() {
           <i className="fas fa-search"></i>
         </button>
       </form>
+      <Select
+        options={options}
+        value={selectedOption}
+        onChange={setSelectedOption}
+        styles={customStyles}
+        isSearchable={false}
+        components={customComponents}
+        placeholder="Sort by"
+      />
     </div>
   );
 }
